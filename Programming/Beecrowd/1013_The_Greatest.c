81e278425ce38dc013b0cef88db90500084661ee
Abdullah9922 <iahtyaq.asif45@gmail.com>
2024-01-30 11:43:13 +0600

    5th day

---
+++ Programming/Beecrowd/1013_The_Greatest.c
@@ -0,0 +1,16 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1013
+#include<stdio.h>
+int main()
+{
+    int a,b,c,s;
+    scanf("%d %d %d",&a,&b,&c);
+
+    x=a+b+y;
+    y=(a*b*c)*(a-b);
+    s=x/2;
+
+
+    //s=((a+b+((a*b*c)*(a-b)))/2);
+    printf("%d eh o maior", s);
+
+}
---
+++ Programming/Beecrowd/1014_Consumption.c
@@ -0,0 +1,12 @@
+//https://www.beecrowd.com.br/judge/en/problems/view/1014
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    scanf("%d %f", &a,&b);
+    printf("%.3f km/l\n", (a/b));
+
+
+}
+
