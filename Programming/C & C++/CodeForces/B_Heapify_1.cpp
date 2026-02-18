2421c9f38a4b9ef83150958a13876f17be110958
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-02-18 14:13:56 +0600

    18-02-2026

--- Programming/C & C++/CodeForces/B_Heapify_1.cpp
+++ Programming/C & C++/CodeForces/B_Heapify_1.cpp
@@ -1,16 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 bool inOrder(vector<int> &v)
 {
-    for(int i = 0; i < v.size()-1; i++){
-        if(v[i] > v[i+1]) return false;
+    for (int i = 0; i < v.size() - 1; i++)
+    {
+        if (v[i] > v[i + 1])
+            return false;
     }
     return true;
 }
-
-
 
 int main()
 {
@@ -23,30 +22,26 @@ int main()
         cin >> n;
 
         vector<int> v(n);
-        for(auto &x : v){
+        for (auto &x : v)
+        {
             cin >> x;
         }
 
-
         for (int i = 0; i < n / 2; i++)
         {
-            if(v[i] > v[2*i+1]){
-                swap(v[i],v[2*i+1]);
+            if (v[i] > v[2 * i + 1])
+            {
+                swap(v[i], v[2 * i + 1]);
             }
-            
         }
 
-        // for (int i = 1; i <= n; i++)
-        // {
-        //     cout << v[i] << " ";
-            
-        // }
-        // cout << endl;
-
-        if(inOrder(v)){
+        if (is_sorted(v.begin(), v.end()))
+        {
             cout << "YES" << endl;
         }
-
-        else cout << "NO" << endl;
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
 }
