142069cd1332b14aabe9599915001eae5a4d61f9
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-02-19 11:34:33 +0600

    19-02-2026

--- Programming/C & C++/CodeForces/B_Heapify_1.cpp
+++ Programming/C & C++/CodeForces/B_Heapify_1.cpp
@@ -1,15 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool inOrder(vector<int> &v)
-{
-    for (int i = 0; i < v.size() - 1; i++)
-    {
-        if (v[i] > v[i + 1])
-            return false;
-    }
-    return true;
-}
 
 int main()
 {
